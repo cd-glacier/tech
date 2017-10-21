eec6deb5650b9e5936493864bb0f42ff6b1ed31a
g-hyoga <hyoga0216@gmail.com>
2017-10-21 17:18:01 +0900

    Initial Commit

---
+++ tech-chatUITests/tech_chatUITests.swift
@@ -0,0 +1,36 @@
+//
+//  tech_chatUITests.swift
+//  tech-chatUITests
+//
+//  Created by glacier on 2017/10/21.
+//  Copyright © 2017年 glacier. All rights reserved.
+//
+
+import XCTest
+
+class tech_chatUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
