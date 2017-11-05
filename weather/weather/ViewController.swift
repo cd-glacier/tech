f85af315e3f492636d5b27de5a92186f25299b41
g-hyoga <hyoga0216@gmail.com>
2017-11-05 14:57:08 +0900

    tenki

--- weather/weather/ViewController.swift
+++ weather/weather/ViewController.swift
@@ -11,15 +11,52 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController {
+    @IBOutlet weak var weatherImg: UIImageView!
+    @IBOutlet weak var backgroundImg: UIImageView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+        let weather = getWeather()
+        print(weather)
+	}
+    
+    func getWeather() -> String {
+        var weather = ""
+        Alamofire.request("https://api.openweathermap.org/data/2.5/weather?APPID=a3318e2703222b96985b824503780a40&q=Osaka").responseJSON { (response) in
+            let data = JSON(response.result.value!)
+            weather = data["weather"][0]["main"].string!
+            print(weather)
+            self.changeWeatherImg(weather: weather)
+        }
+        return weather
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func changeWeatherImg(weather: String) {
+        switch weather {
+        case "Clear":
+            weatherImg.image = UIImage(named: "sun.png")
+            backgroundImg.image = UIImage(named: "sun-background.jpg")
+        case "Clouds": 
+            weatherImg.image = UIImage(named: "cloud.png")
+            backgroundImg.image = UIImage(named: "cloud-background.jpg")
+        case "Rain":
+            weatherImg.image = UIImage(named: "rain.png")
+            backgroundImg.image = UIImage(named: "rain-background.jpg")
+        default:
+            weatherImg.image = UIImage(named: "sun.png")
+            backgroundImg.image = UIImage(named: "sun-background.jpg")
+        }
     }
+    
+    @IBAction func pushReloadBtn(_ sender: Any) {
+        getWeather()
+    }
+    
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 
 
 }
