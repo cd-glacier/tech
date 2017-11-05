10e176d717509c7dba74c1af85cedf05574e413d
g-hyoga <hyoga0216@gmail.com>
2017-11-05 12:11:04 +0900

    add alamofire and swiftyJSON

---
+++ weather/weather/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by glacier on 2017/11/05.
+//  Copyright © 2017年 glacier. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
